29a79ed16327a9c0851080c4815882043f97845c
Fati Iseni <fati.iseni@pozitrongroup.com>
2024-10-18 11:57:41 +0200

    Added tests.

---
+++ tests/Pozitron.Extensions.MediatR.Tests/ExtendedMediatorTests.cs
@@ -0,0 +1,60 @@
+﻿namespace Tests;
+
+// Testing just the edge cases. The rest is covered by the publisher tests.
+public class ExtendedMediatorTests
+{
+    private readonly TestQueue<int> _queue = new();
+
+    [Fact]
+    public async Task PublishExtension_ThrowsNotSupported_GivenExtendedMediatorNotRegistered()
+    {
+        var services = new ServiceCollection();
+        services.AddMediatR(x =>
+        {
+            x.RegisterServicesFromAssemblyContaining<Ping>();
+        });
+        using var scope = services.BuildServiceProvider().CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+        var sut = async () => await mediator.Publish(new Ping(), PublishStrategy.Sequential);
+        await sut.Should().ThrowExactlyAsync<NotSupportedException>();
+    }
+
+    [Fact]
+    public async Task Publish_FallsBackToDefault_GivenInvalidStrategy()
+    {
+        var services = new ServiceCollection();
+        services.AddSingleton(_queue);
+        services.AddExtendedMediatR(typeof(Ping));
+        using var scope = services.BuildServiceProvider().CreateScope();
+        var mediator = scope.ServiceProvider.GetRequiredService<IMediator>();
+
+        await mediator.Publish(new Ping(), (PublishStrategy)100);
+        _queue.Write(0);
+
+        var result = _queue.GetValues();
+        result.Should().Equal(Pong1.Id, Pong3.Id, 0);
+    }
+
+    public record Ping() : INotification { }
+
+    public class Pong1(TestQueue<int> queue) : INotificationHandler<Ping>
+    {
+        public const int Id = 1;
+        public async Task Handle(Ping notification, CancellationToken cancellationToken)
+        {
+            // Adding a delay at the start compared to Pong3. We still expect this to be the first message.
+            await Task.Delay(20, cancellationToken);
+            queue.Write(Id);
+        }
+    }
+    public class Pong3(TestQueue<int> queue) : INotificationHandler<Ping>
+    {
+        public const int Id = 3;
+        public async Task Handle(Ping notification, CancellationToken cancellationToken)
+        {
+            await Task.Yield();
+            queue.Write(Id);
+        }
+    }
+}
---
+++ tests/Pozitron.Extensions.MediatR.Tests/MediatorExtensionsTests.cs
@@ -0,0 +1,108 @@
+﻿namespace Tests;
+
+public class MediatorExtensionsTests
+{
+    [Fact]
+    public void AddExtendedMediatR_GivenLifetimeAndTypes()
+    {
+        var services = new ServiceCollection();
+        services.AddExtendedMediatR(ServiceLifetime.Scoped, typeof(Ping));
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Scoped);
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddExtendedMediatR_GivenTypes()
+    {
+        var services = new ServiceCollection();
+        services.AddExtendedMediatR(typeof(Ping));
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Transient); // Default one
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddExtendedMediatR_GivenLifetimeAndAssemblies()
+    {
+        var services = new ServiceCollection();
+        services.AddExtendedMediatR(ServiceLifetime.Scoped, typeof(Ping).Assembly);
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Scoped);
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddExtendedMediatR_GivenAssemblies()
+    {
+        var services = new ServiceCollection();
+        services.AddExtendedMediatR(typeof(Ping));
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Transient); // Default one
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddExtendedMediatR_GivenLambdaConfiguration()
+    {
+        var services = new ServiceCollection();
+        services.AddExtendedMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssemblyContaining<Ping>();
+            cfg.Lifetime = ServiceLifetime.Scoped;
+        });
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Scoped);
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    [Fact]
+    public void AddExtendedMediatR_GivenConfiguration()
+    {
+        var services = new ServiceCollection();
+        var cfg = new MediatRServiceConfiguration();
+        cfg.RegisterServicesFromAssemblyContaining<Ping>();
+        cfg.Lifetime = ServiceLifetime.Scoped;
+        services.AddExtendedMediatR(cfg);
+
+        var mediatorDescriptor = services.First(x => x.ServiceType == typeof(IMediator));
+        mediatorDescriptor.ImplementationType.Should().Be<ExtendedMediator>();
+        mediatorDescriptor.Lifetime.Should().Be(ServiceLifetime.Scoped);
+
+        services.FirstOrDefault(x => x.ServiceType == typeof(IRequestHandler<Ping>)).Should().NotBeNull();
+        services.FirstOrDefault(x => x.ServiceType == typeof(INotificationHandler<Pinged>)).Should().NotBeNull();
+    }
+
+    public record Ping() : IRequest { }
+    public class Pong() : IRequestHandler<Ping>
+    {
+        public Task Handle(Ping request, CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+    public record Pinged() : INotification { }
+    public class Ponged() : INotificationHandler<Pinged>
+    {
+        public Task Handle(Pinged notification, CancellationToken cancellationToken)
+            => Task.CompletedTask;
+    }
+}
